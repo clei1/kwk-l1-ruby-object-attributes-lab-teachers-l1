b6c5010d4cf2fd1f5dfdd8609069bbcadf49438a
clei1 <callmecon328@gmail.com>
2018-07-21 17:28:08 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
 # person.rb
+
+class Person
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(dog_breed)
+    @job = dog_breed
+  end
+
+  def job
+    @job
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
